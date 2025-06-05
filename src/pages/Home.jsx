7f8c6345cc8628c6b84ab87eaa6fbcf30db24496
Ivan Boichuk <ivan.boychuck@gmail.com>
2025-06-05 23:07:31 +0200

    Fix: srxSet

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import { NavLink } from "react-router-dom";
 const Home = () => <section className="relative h-full flex items-center">
-    <img src="/public/offer-bg.jpg" srcSet="/public/offer-bg@2x.jpg, 2x" alt="Yelow camper near the lake" className="absolute inset-0 size-full object-cover -z-20" />
+    <img src="/public/offer-bg.jpg" srcSet="/public/offer-bg@2x.jpg 2x" alt="Yelow camper near the lake" className="absolute inset-0 size-full object-cover -z-20" />
     <div className="absolute inset-0 size-full object-cover -z-10 bg-black/20"></div>
     <div className="container font-semibold text-theme-inputs leading-8">
         <h1 className="text-5xl mb-4">Campers of your dreams</h1>
